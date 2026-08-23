702b2a5afb50475b8ac0062672fe89c46e55c155
HGUFlyingDog <suidonghui666@gmail.com>
2026-08-24 00:47:06 +0800

    fix(logger) 更新参数的命名 增加do while(0)的包裹

--- logger.hpp
+++ logger.hpp
@@ -8,16 +8,18 @@
 #define ERR 2
 #define DEFAULT_LOG_LEVEL INFO
 
-#define LOG(level, formart, ...)                                                            \
-    if (level >= DEFAULT_LOG_LEVEL)                                                         \
-    {                                                                                       \
-        time_t t = time(NULL);                                                              \
-        struct tm *lt = localtime(&t);                                                      \
-        char buf[32] = {0};                                                                 \
-        strftime(buf, 31, "%H:%M:%S", lt);                                                  \
-        fprintf(stdout, "[%s %s:%d]" formart "\n", buf, __FILE__, __LINE__, ##__VA_ARGS__); \
-    }
+#define LOG(level, format, ...)                                                                \
+    do                                                                                          \
+        if (level >= DEFAULT_LOG_LEVEL)                                                         \
+        {                                                                                       \
+            time_t t = time(NULL);                                                              \
+            struct tm *lt = localtime(&t);                                                      \
+            char buf[32] = {0};                                                                 \
+            strftime(buf, 31, "%H:%M:%S", lt);                                                  \
+            fprintf(stdout, "[%s %s:%d]" format "\n", buf, __FILE__, __LINE__, ##__VA_ARGS__); \
+        }                                                                                       \
+    while (0);
 
-#define INFO_LOG(formart, ...) LOG(INFO, formart, ##__VA_ARGS__)
-#define DBG_LOG(formart, ...) LOG(DBG, formart, ##__VA_ARGS__)
-#define ERR_LOG(formart, ...) LOG(ERR, formart, ##__VA_ARGS__)
+#define INFO_LOG(format, ...) LOG(INFO, format, ##__VA_ARGS__)
+#define DBG_LOG(format, ...) LOG(DBG, format, ##__VA_ARGS__)
+#define ERR_LOG(format, ...) LOG(ERR, format, ##__VA_ARGS__)
