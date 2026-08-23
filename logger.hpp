b3b0cade06bbdf651d38c97d7f921412a76b2bf2
HGUFlyingDog <suidonghui666@gmail.com>
2026-08-24 00:56:53 +0800

    fix(logger) 删除宏函数后边的分号 交给使用者自行添加分号 增加大括号

--- logger.hpp
+++ logger.hpp
@@ -9,16 +9,17 @@
 #define DEFAULT_LOG_LEVEL INFO
 
 #define LOG(level, format, ...)                                                                \
-    do                                                                                          \
-        if (level >= DEFAULT_LOG_LEVEL)                                                         \
-        {                                                                                       \
-            time_t t = time(NULL);                                                              \
-            struct tm *lt = localtime(&t);                                                      \
-            char buf[32] = {0};                                                                 \
-            strftime(buf, 31, "%H:%M:%S", lt);                                                  \
+    do                                                                                         \
+    {                                                                                          \
+        if (level >= DEFAULT_LOG_LEVEL)                                                        \
+        {                                                                                      \
+            time_t t = time(NULL);                                                             \
+            struct tm *lt = localtime(&t);                                                     \
+            char buf[32] = {0};                                                                \
+            strftime(buf, 31, "%H:%M:%S", lt);                                                 \
             fprintf(stdout, "[%s %s:%d]" format "\n", buf, __FILE__, __LINE__, ##__VA_ARGS__); \
-        }                                                                                       \
-    while (0);
+        }                                                                                      \
+    } while (0)
 
 #define INFO_LOG(format, ...) LOG(INFO, format, ##__VA_ARGS__)
 #define DBG_LOG(format, ...) LOG(DBG, format, ##__VA_ARGS__)
